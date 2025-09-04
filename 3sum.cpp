493b3f5ff0c9a53f62b4712baae9d3f8db050ae0
Rajat037 <rajatshukla370@gmail.com>
2025-09-05 00:43:02 +0530

    Update 3sum.cpp

--- 3sum.cpp
+++ 3sum.cpp
@@ -1,28 +1,50 @@
 #include<iostream>
 using namespace std;
 
-void threesum(int arr[],int n,int target){
-    for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                for(int k=j+1;k<n;k++){
-                    if(arr[i]+arr[j]+arr[k] == target ){
-                        cout<<arr[i]<<","<<arr[j]<<','<<arr[k]<<endl;
-                        
-                    }
+vector<vector<int>> threesum(vector<int>& nums,int target) {
+        sort(nums.begin(),nums.end());
+        vector<vector<int>> ans;
+        for(int i=0; i<nums.size(); i++){
+
+            if(i>0 && nums[i]==nums[i-1]) continue;
+            int left = i+1;
+            int right = nums.size()-1;
+
+            while(left<right){
+                int sum = nums[i]+nums[left]+nums[right];
+
+                if(sum == target){
+                    ans.push_back({nums[i],nums[left],nums[right]});
+                    left++;
+                    right--;
+
+                    while( left<right &&nums[left] == nums[left-1]) left++;
+                    while( left<right &&nums[right] == nums[right+1]) right--;
                     
                 }
+                else if(sum<target) left++;
+
+                else right--;
             }
+           
+        }
+        return ans;
     }
-    cout<<"xyz";
-}
  
 int main(){
 
     int arr[]={10,20,30,40};
-    int size=4;
-    int target=70;
+    vector<int>nums = {-1,0,1,2,-1,-4};
+    int target=0;
  
-    threesum(arr,size,target);
- 
+    vector<vector<int>> ans= threesum(nums,target);
+    for (int i = 0; i < ans.size(); i++) {
+        cout << "[";
+        for (int j = 0; j < ans[i].size(); j++) {
+            cout << ans[i][j];
+            if (j < ans[i].size() - 1) cout << ", ";
+        }
+        cout << "]" << endl;
+    }
     return 0;
 }
